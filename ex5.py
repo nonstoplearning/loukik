64f4b5c628e1ef8a250812fda05731d1e034c39c
nonstoplearning <non.learning@gmail.com>
2020-04-17 05:43:22 +0530

    ex5

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Loukik H Huilgolkar'
+my_age = 28 # not a lie
+my_height = 176 #cms
+my_weight = 79 #kgs
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'Brown'
+
+print ("let's talk about %s " % my_name)
+print ("He's %d centimeters tall" % my_height)
+print ("He's %d kilograms heavy" % my_weight)
+print ("Actually thats not too heavy")
+print ("He's got %s eyes and %s hair" %(my_eyes, my_hair))
+print ("his teeth are usually %s depending on the coffee" % my_teeth)
+
+
+#this line is tricky, tr to get it exactly right
+print ("If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight))
