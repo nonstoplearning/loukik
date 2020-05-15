adc644cec62c993722b9bcc9cc3f9f92749ed256
nonstoplearning <non.learning@gmail.com>
2020-05-15 07:17:58 +0530

    10, 11 & 12

---
+++ ex11.py
@@ -0,0 +1,6 @@
+age = input("how old are you?")
+height = input("how tall are you?")
+weight = input ("how much do you weigh?")
+
+print ("so, you are %r old, %r tall and %r heavy." %(
+age, height, weight))
